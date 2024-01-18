0b7bd27d9b4e0fbd8b7e857255d4c9adb992e71c
sam <samuel13241@gmail.com>
2024-01-18 13:42:35 -0800

    no message

--- src/app/_env/env.now.ts
+++ src/app/_env/env.now.ts
@@ -1,5 +1,5 @@
 const dev_environment_url = 'http://localhost:8080'
-const prod_environment_url = 'ec2-44-242-115-211.us-west-2.compute.amazonaws.com'
+const prod_environment_url = 'http://ec2-44-242-115-211.us-west-2.compute.amazonaws.com:8080'
 
 
 export const envNow = prod_environment_url;
