747f611921eadcddc5887f8282a0df2a77ad7fc3
sam <samuel13241@gmail.com>
2024-01-25 14:24:00 -0800

    Google, Faceboot Oauth2 dependencies Added. After you pull the commit, please run   'npm install' command to download added dependencies to your local project

--- src/app/_env/env.now.ts
+++ src/app/_env/env.now.ts
@@ -2,5 +2,12 @@ const dev_environment_url = 'http://localhost:8080'
 const prod_environment_url = 'http://ec2-44-242-115-211.us-west-2.compute.amazonaws.com:8080'
 
 
-export const envNow = prod_environment_url;
+export const env_var= {
+  envNow : prod_environment_url,
+  google_api_id: '831783881236-9arduq0oaj4gvjt4cjmcrt2j08ojlerj.apps.googleusercontent.com',
+  facebook_api_id: '6898865823545565'
+}
+
+
+
 
