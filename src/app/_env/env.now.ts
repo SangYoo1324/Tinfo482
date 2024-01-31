0521f1cd42f57438d1e1364a76300213790e16b9
sam <samuel13241@gmail.com>
2024-01-30 21:59:13 -0800

    just chnged backend server addr

--- src/app/_env/env.now.ts
+++ src/app/_env/env.now.ts
@@ -1,7 +1,7 @@
 const dev_environment_url = 'http://localhost:8080'
-const prod_environment_url = 'http://ec2-44-242-115-211.us-west-2.compute.amazonaws.com:8080'
+const prod_environment_url = 'http://ec2-100-21-105-141.us-west-2.compute.amazonaws.com'
 
-
+//http://ec2-44-242-115-211.us-west-2.compute.amazonaws.com:8080
 export const env_var= {
   envNow : prod_environment_url,
   google_api_id: '831783881236-9arduq0oaj4gvjt4cjmcrt2j08ojlerj.apps.googleusercontent.com',
