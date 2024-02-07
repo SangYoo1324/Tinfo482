59fd6dec2f7749f2eb1cfb81c43db8255226e547
sam <samuel13241@gmail.com>
2024-02-07 01:17:19 -0800

    prof + items

--- src/app/pages/flowers/flowers.component.ts
+++ src/app/pages/flowers/flowers.component.ts
@@ -7,13 +7,85 @@ import { Component } from '@angular/core';
     [title]="'Flowers'"
     [subTitle]="'Pick & up flowers you want to buy..'"
     ></app-page-title>
+
+    <section>
+
+      <div class="container">
+
+        <h1>Send Flowers</h1>
+        <div class="info-wrap">
+            <div class="numbers"><p>17 items</p></div> <div class="horizontal-bar"><p></p></div>
+
+          <div class="select-wrap">
+            <div class="second-wrap">
+              <span>FILTER &nbsp;<i class="fa-solid fa-angle-down"></i></span>
+              <select>
+                <option value="default">ALL</option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+    </section>
+    <div class="container">
+      <div class="row">
+        <div *ngFor="let item of itemObjectArray let i=index" class="col-lg-3 box">
+          <app-item-card *ngIf=" i !==3"></app-item-card>
+          <div class="placeholder" *ngIf="i ===3">
+            <div class="wrap">
+              <p>Looking for same day flower delivery?
+                Call our Shop to speak to a florist!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
   `,
   styleUrl: './flowers.component.css'
 })
 export class FlowersComponent {
 
 
-
+  itemObjectArray = [
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1},
+    {
+      name: "aaaaaa",
+      price: 1}
+  ]
 
 
 }
--- src/app/pages/flowers/item-card/item-card.component.ts
+++ src/app/pages/flowers/item-card/item-card.component.ts
@@ -3,9 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-item-card',
   template:`
+    <div class="card-wrap">
+      <div class="top">
+        <div class="local-delivery">
+          <div class="button">local delivery & pick-up &nbsp;<i class="fa-solid fa-taxi"></i></div>
+        </div>
+        <div class="cover-overlay"></div>
+      </div>
+      <div class="bottom">
+        <div class="title">ZZ Plant</div>
+        <div class="price"><span><b>From</b></span> &nbsp; <span>$6.50</span></div>
+      </div>
+    </div>
   `,
   styleUrl: './item-card.component.css'
 })
 export class ItemCardComponent {
+
 
 }
